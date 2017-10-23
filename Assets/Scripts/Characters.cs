5f5db3be3bc21aeb285b53bf3b2fe035d3c06875
Daltik <33038659+Daltik@users.noreply.github.com>
2017-10-23 22:00:09 +0200

    commit

---
+++ Assets/Scripts/Characters.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Characters : MonoBehaviour{
+
+	protected string name;
+	protected int health;
+	protected bool alive;
+	private string textStats;
+
+	protected List<string> statsList = new List<string>();
+
+	public void attack(){
+		
+	}
+
+	public void damage(){
+
+	}
+
+	public void death(){
+
+	}
+
+	public List<string> characterStats(string name, int health, bool alive){
+		statsList.Add("Name: " + name);
+		statsList.Add("Health: " + health.ToString());
+		statsList.Add("Alive: " + alive.ToString());
+		
+		return statsList;
+
+	}
+
+	public string printStats(List<string> list){
+				
+
+		for (int i = 0; i < list.Count; i++){
+				textStats =  textStats + list[i] + "\n";
+				
+		}
+	
+		return textStats;
+	}
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+
+	private List<string> statsListPlayer;
+	private List<string> statsListEnemy;
+
+	private string textStats;
+
+	public Text txtPlayerStats;
+	public Text txtEnemyStats;
+
+
+	// Use this for initialization
+	void Start () {
+	//	printStats();
+		
+		Player player1 = new Player();
+		Enemy enemy1 = new Enemy();
+	
+		
+		statsListPlayer = new List<string>(player1.characterStats("John", 100, true));
+		statsListEnemy = enemy1.characterStats("Eddie", 150, true);
+
+		
+		txtPlayerStats.text = "Player Stats: \n \n" + player1.printStats(statsListPlayer);
+		txtEnemyStats.text = "Enemy Stats: \n \n" + enemy1.printStats(statsListEnemy);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	
+
+	
+
+}
